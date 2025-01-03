fc8b14ba1dedaa85d26bb290e94b1137ab32d69c
siddharthvishwamitra <sidhrthkmr@gmail.com>
2025-01-03 10:48:02 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './header';
 import Footer from './footer';
 import Home from './home';
